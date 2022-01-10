33b04a6b697ecab4f4f73da60da123f3f46efc64
tnaake <thomasnaake@gmx.de>
2022-01-10 16:04:18 +0100

    Create testthat.R

---
+++ tests/testthat.R
@@ -0,0 +1,9 @@
+library("testthat")
+library("MsQuality")
+library("MsQualityUtils")
+library("Spectra")
+library("MsExperiment")
+library("msdata")
+library("S4Vectors")
+
+test_check("MsQualityUtils")
